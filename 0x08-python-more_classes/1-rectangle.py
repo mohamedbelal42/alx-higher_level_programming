026ae16198f290271d57046e6bceca098f7fc0c1
mohamedbelal42 <hamooo12345x@gmail.com>
2024-02-12 12:28:33 -0800

    rectangle1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle."""
-
+"""Defining an empty class Rectangle."""
 
 class Rectangle:
+    """Empty representation of a Rectangle."""
     def __init__(self, width=0, height=0):
         """initializes the rectangle."""
         self.height = height
